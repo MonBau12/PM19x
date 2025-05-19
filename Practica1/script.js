f0428761d5185c01f5e205508dcc31b3c340ed15
MonBau12 <montse.bau.sandoval@gmail.com>
2025-05-19 17:02:24 -0600

    Primer commit

---
+++ Practica1/script.js
@@ -0,0 +1,18 @@
+// EJERCICIO A: Uso de let y const
+
+const nombre = "Montse";
+const edad = 21;
+const mensaje = `Hola ${nombre}, tienes ${edad} años.`;
+console.log(mensaje);
+
+// EJERCICIO B: Función flecha
+
+const suma = (a, b) => a + b;
+console.log(`La suma de 5 + 3 es: ${suma(5, 3)}`);
+
+
+//Práctica 1 - Variables y Funciones
+//Ejercicio A
+//Se usó `const` para variables que no cambian y `let` cuando sí podrían cambiar.
+//Ejercicio B
+//Se convirtió una función tradicional en una arrow function.
